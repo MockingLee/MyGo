b2136c03b2a2ae4f8b5903d13733de4ae4ae848b
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-06-24 09:59:17 +0800

    add readme

--- main.go
+++ main.go
@@ -1,27 +1,10 @@
 package main
 
-import (
-    "html/template"
-    "net/http"
-)
-
-// User struct
-type User struct {
-    Username string
-}
+import "fmt"
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        user := User{Username: "bonfy"}
-        tpl, _ := template.New("").Parse(`<html>
-            <head>
-                <title>Home Page - Bonfy</title>
-            </head>
-            <body>
-                <h1>Hello, {{.Username}}!</h1>
-            </body>
-        </html>`)
-        tpl.Execute(w, &user)
-    })
-    http.ListenAndServe(":8888", nil)
+	a := 10
+	if a > 1 {
+		fmt.Println("hh")
+	}
 }
---
+++ readme.md
@@ -0,0 +1,15 @@
+# go 
+- 没有继承  匿名字段
+- 指针
+- 函数/方法绑定
+- code fmt  :    ```go fmt main.go```
+- switch  不需要break 每一个case都是独立的
+- 没有while 
+
+```
+for{
+
+}
+```
+
+
