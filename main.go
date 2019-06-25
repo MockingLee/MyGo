70980a3d59b905e4afdf104a3bd7bf1641cb2edb
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-06-25 08:13:25 +0800

    update readme

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 
+func first() {
+	defer fmt.Println("first")
+	fmt.Println("first func run")
+}
+func second() {
+	defer fmt.Println("second")
+	fmt.Println("second func run")
+}
+
 func main() {
-	a := 10
-	if a > 1 {
-		fmt.Println("hh")
-	}
+	var x int
+	var x_ptr *int
+
+	x = 10
+	x_ptr = &x
+
+	fmt.Println(&x_ptr)
 }
--- readme.md
+++ readme.md
@@ -4,12 +4,15 @@
 - 函数/方法绑定
 - code fmt  :    ```go fmt main.go```
 - switch  不需要break 每一个case都是独立的
-- 没有while 
+- 没有while for代替
 
 ```
+// 等同于while(true)
 for{
 
 }
 ```
+
+- 数组切片
 
 
