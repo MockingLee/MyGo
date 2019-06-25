a5f69bcc2acc2f83c1e94a5c420523414d40c9e1
MockingLee <38075220+MockingLee@users.noreply.github.com>
2019-06-25 09:45:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func first() {
-	defer fmt.Println("first")
-	fmt.Println("first func run")
+func fixed_shooting(msg_chan chan string) {
+	var times = 3
+	var t = 1
+	for {
+		if t <= times {
+			msg_chan <- "fixed shooting"
+		}
+		t++
+		time.Sleep(time.Second * 1)
+	}
 }
-func second() {
-	defer fmt.Println("second")
-	fmt.Println("second func run")
+
+func three_point_shooting(msg_chan chan string) {
+	var times = 5
+	var t = 1
+	for {
+		if t <= times {
+			msg_chan <- "three point shooting"
+		}
+		t++
+		time.Sleep(time.Second * 1)
+	}
 }
 
 func main() {
-	var x int
-	var x_ptr *int
+	c_fixed := make(chan string)
+	c_3_point := make(chan string)
 
-	x = 10
-	x_ptr = &x
+	go fixed_shooting(c_fixed)
+	go three_point_shooting(c_3_point)
 
-	fmt.Println(&x_ptr)
+	go func() {
+		for {
+			select {
+			case msg1 := <-c_fixed:
+				fmt.Println(msg1)
+			case msg2 := <-c_3_point:
+				fmt.Println(msg2)
+			case <-time.After(time.Second * 5):
+				fmt.Println("timeout, check again...")
+			}
+		}
+
+	}()
+
+	var input string
+	fmt.Scanln(&input)
 }
